2a85c60718df392886dcb32f0ef096bcbeb7dac5
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-11-01 18:33:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,12 @@ class Triangle
   end
 
   def kind
-    
+    if @l1 + @l2 !> @l3 && @l1 == 0 || @l2 == 0 || @l3 == 0 
+      begin 
+        raise TriangleError
+      rescue TriangleError => error
+
+      end 
   end
 
 end
