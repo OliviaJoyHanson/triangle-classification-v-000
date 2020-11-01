3ef232bd2e5ae75df33cb8497a3453c8c52091a4
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-11-01 18:36:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,14 +9,14 @@ class Triangle
 
   def kind
     if @l1 + @l2 !> @l3 && @l1 == 0 || @l2 == 0 || @l3 == 0
-      raise TriangleError 
-    elsif @l1 == @l2 && @l1 == @l3 
-      :equilateral 
-    elsif @l1 == @l2 || @l1 == @l3 || @l2 == @l3 
+      raise TriangleError
+    elsif @l1 == @l2 && @l1 == @l3
+      :equilateral
+    elsif @l1 == @l2 || @l1 == @l3 || @l2 == @l3
       :isosceles
-    else 
+    else
       :scalene
-    end 
+    end
   end
 
 end
