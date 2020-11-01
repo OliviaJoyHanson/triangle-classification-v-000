0b2ca68fbc75af4b4a8fb935c4e7b66d886b1523
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-11-01 18:24:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,26 +1,10 @@
 class Triangle
-  attr_accessor :side_1, :side_2, :side_3
+  attr_accessor :l1, :l2, :l3
 
-  def initialize(side_1, side_2, side_3 )
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
   end
-
-  def kind
-    if @side_1 + @side_2 > @side_3 && @side_1 + @side_3 > @side_2 && @side_2 + @side_3 > @side_1 && @side_1 > 0 && @side_2 > 0 && @side_3 > 0
-      if @side_1 == @side_2 && @side_2 == @side_3
-         :equilateral
-      elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
-         :isosceles
-      else
-        :scalene
-      end
-    else
-      raise TriangleError
-    end
-  end
-
-  class TriangleError < StandardError
-  end
+  
 end
